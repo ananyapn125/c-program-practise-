89e92b983a79ed9dde1b0a95316705af7e7d9eb6
ananyapn125 <ananyapn125@gmail.com>
2026-05-12 23:21:55 +0530

    Create aplhabet detection.c
    While practicing this program, I learned that in a switch statement, the case values do not need to be written in sequential order. The program works correctly as long as the matching case value exists.

---
+++ aplhabet detection.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+  //constant or vowel detection 
+  char ch;
+  int count;
+  count=0;
+  printf("enter a alphabet to check if its a vowel or a consonant");
+  scanf("%s", &ch);
+  if(ch=='a' || ch=='e' ||  ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E'||
+  ch=='I' || ch=='O' || ch=='U')
+  {
+    count++;
+  }
+  
+  switch(count)
+  {
+      case 1:
+        printf("The entered alphabet is a vowel");
+        break;
+      case 0:
+         printf("The entered alphabet is a consonant");
+         break;
+  }
+  return 0;
+}
